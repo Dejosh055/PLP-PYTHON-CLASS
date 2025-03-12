cfbcccc7803d14315879eff3ab0a07ee876fea3e
Dejoah055 <Dejosh055@gmail.com>
2025-03-11 15:09:57 -1100

    my mini calculator

---
+++ PLP-Py-class day 1/index.py
@@ -0,0 +1,23 @@
+print('Choose operation')
+print('1. ADD')
+print('2. MULTIPLICATIO')
+print('3. SUBTRACTION')
+print('4. DIVISIION')
+
+operation = input()
+num1 = input('Enter first number:')
+num2 = input('Enter Second number:')
+if operation == '1':
+    print('the sum is'+ " " + str(int( num1) + int(num2)) )
+
+elif operation == '2':
+    print('the product is'+ " " + str(int( num1) * int(num2)) )
+
+elif operation == '3':
+    print('the Difference is'+ " " + str(int( num1) - int(num2)) )
+elif operation == '4':
+    print('the Result is'+ " " + str(int( num1) / int(num2)) )
+
+else:
+    print('enter a valid number')
+       
